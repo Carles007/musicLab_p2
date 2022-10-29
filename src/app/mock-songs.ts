a100aedefe6d4e940a73c39249df8bc890da388f
JacobRC <jakomania@gmail.com>
2022-10-29 14:06:23 +0200

    Retoque iconos y tamaño fuentes player

--- src/app/mock-songs.ts
+++ src/app/mock-songs.ts
@@ -2,10 +2,44 @@ import { Song } from "./song";
 
 export const SONGS: Song[] = [
 
-  { id: 14, name: 'Seek & Destroy', artist:'Metallica', album: 'Kill \'Em All', year: 1983, time: '6:54', imagen:'assets/img/metallica.png', url: 'assets/mp3/Seek & Destroy.mp3'},
-  { id: 1, name: 'The Trooper', artist:'Iron Maiden', album: 'Piece of Mind', year: 1983, time: '4:16', imagen:'assets/img/ironmaiden.png', url: 'assets/mp3/The Trooper.mp3'},
-  { id: 2, name: 'The Fat Man', artist:'Fats Domino', album: 'Fats Domino Swings', year: 1959, time: '2:36', imagen:'assets/img/fatsdomino.jpg', url: 'assets/mp3/The Fat Man.mp3'},
-  { id: 3, name: 'Breathe', artist:'Pink Floyd', album: 'Dark Side of the Moon', year: 1973, time: '4:02', imagen:'assets/img/pink-floyd.jpg', url: 'assets/mp3/Breathe.mp3'},
+  { 
+    id: 14,
+    name: 'Seek & Destroy',
+    artist:'Metallica',
+    album: 'Kill \'Em All', 
+    year: 1983, time: '6:54', 
+    imagen:'assets/img/metallica.png', 
+    url: 'assets/mp3/Seek & Destroy.mp3'
+  },
+  { 
+    id: 1,
+    name: 'The Trooper', 
+    artist:'Iron Maiden', 
+    album: 'Piece of Mind', 
+    year: 1983, time: '4:16', 
+    imagen:'assets/img/ironmaiden.png', 
+    url: 'assets/mp3/The Trooper.mp3'
+  },
+  { 
+    id: 2,
+    name: 'The Fat Man', 
+    artist:'Fats Domino', 
+    album: 'Fats Domino Swings', 
+    year: 1959, 
+    time: '2:36', 
+    imagen:'assets/img/fatsdomino.jpg', 
+    url: 'assets/mp3/The Fat Man.mp3'
+  },
+  { 
+    id: 3,
+    name: 'Breathe', 
+    artist:'Pink Floyd', 
+    album: 'Dark Side of the Moon', 
+    year: 1973, 
+    time: '4:02', 
+    imagen:'assets/img/pink-floyd.jpg', url: 'assets/mp3/Breathe.mp3'
+  },
+
   { id: 4, name: 'Show Must Go On', artist:'Queen', album: 'Innuendo', year: 1991, time: '4:22', imagen:'assets/img/Queen.jpg', url: 'assets/mp3/The Show Must Go On.mp3'},
   { id: 41, name: 'Bohemian Rhapsody ', artist:'Queen', album: ' A Night at the Opera', year: 1975, time: '5:59', imagen:'assets/img/Queen.jpg', url: 'assets/mp3/Bohemian Rhapsody.mp3'},
   { id: 5, name: 'Black Dog', artist:'Led Zeppelin', album: 'Led Zeppelin', year: 1971, time: '4:55', imagen:'assets/img/ledzeppelin.jpeg', url: 'assets/mp3/Black Dog.mp3'},
